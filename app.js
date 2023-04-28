510f77a1a33038a649cdca8f98630be5137c16a1
Fernando Valenzuela <99852078+FernValenzuela@users.noreply.github.com>
2023-04-27 20:00:44 -0500

    Updated JS
    Added weatherapi to JS

--- app.js
+++ app.js
@@ -7,7 +7,13 @@ window.addEventListener('load', ()=> {
         (position =>{
             long = position.longitude;
             lat = position.coords.latitude;
+
+            const api = 'https://api.weatherapi.com/v1/forecast.json?key=11210757a88e4b2898a04644232804&q=79045&days=7&aqi=yes&alerts=yes
+            '
         });
+
+        fetch(api)
+            .then(data)
     }else{
         h1.textContent = "Uh oh something went wrong."
     }
