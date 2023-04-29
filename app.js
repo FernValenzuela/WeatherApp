90c671cc1783e05ba3cc7db009f08d3cfa251346
Fernando Valenzuela <99852078+FernValenzuela@users.noreply.github.com>
2023-04-28 20:25:03 -0500

    Update app.js

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ window.addEventListener('load', ()=> {
             long = position.longitude;
             lat = position.coords.latitude;
 
-            const api = 'https://api.weatherapi.com/v1/forecast.json?key=11210757a88e4b2898a04644232804&q=79045&days=7&aqi=yes&alerts=yes
-            '
+            const api = 'https://api.weatherapi.com/v1/forecast.json?key=11210757a88e4b2898a04644232804&q=79045&days=7&aqi=yes&alerts=yes';
+            
         });
 
         fetch(api)
